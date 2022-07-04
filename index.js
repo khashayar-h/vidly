16fc3e8701dffb185626f8cd5f837b7bcd2cc748
Khashayar Haghighi <xerxes.2000a@gmail.com>
2022-07-04 17:33:12 +0430

    Added movies and routers API

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const genresRouter = require('./routes/genres');
+const moviesRouter = require('./routes/movies');
 const customersRouter = require('./routes/customers');
+const rentalsRouter = require('./routes/rentals');
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/vidly')
@@ -10,7 +12,9 @@ mongoose.connect('mongodb://localhost/vidly')
 
 app.use(express.json());
 app.use('/api/genres', genresRouter);
-app.use('/api/customers',customersRouter)
+app.use('/api/customers',customersRouter);
+app.use('/api/movies', moviesRouter);
+app.use('/api/rentals', rentalsRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
